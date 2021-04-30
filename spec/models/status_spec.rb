d71145dfb8d01eaec4a810285f7005ad26e4d04f
Shaher-11 <shahershamroukh@gmail.com>
2021-04-30 11:40:12 +0200

    Fix post model error

--- spec/models/status_spec.rb
+++ spec/models/status_spec.rb
@@ -1,3 +1,12 @@
+# == Schema Information
+#
+# Table name: statuses
+#
+#  id         :bigint           not null, primary key
+#  text       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 require 'rails_helper'
 
 RSpec.describe Status, type: :model do

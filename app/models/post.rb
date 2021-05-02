b80b72dd21539ec82e6bc99200f6447745f6a9e7
Shaher-11 <shahershamroukh@gmail.com>
2021-05-02 15:16:23 +0200

    Render timlines

--- app/models/post.rb
+++ app/models/post.rb
@@ -26,4 +26,8 @@ class Post < ApplicationRecord
   has_many :replies, class_name: "Post", foreign_key: :thread_id
   has_many :pictures
   scope :not_reply, -> { where(thread_id: nil) }
+  scope :written_by, -> (username) {
+    poster = User.find_by_username(username)
+    where(user: poster)
+  }
 end
